8745cfed28b2e0310037353f2578517f00b6eb18
zhaoliang <kybetter@163.com>
2019-01-28 16:51:03 +0800

    feat: 一个简单的拖拽和缩放

---
+++ 003-grag-resize/index.js
@@ -0,0 +1,57 @@
+class SimpleDragResize {
+  constructor(props) {
+    this.el = props;
+    this.el.style.setProperty("transform", "translate(0px, 0px)");
+    this.el.onmousedown = this.onMouseDown.bind(this);
+  }
+
+  getXY(data) {
+    const res = /translate\((\d+)px, (\d+)px\)/.exec(data);
+    return res && [res[1], res[2]] || [0, 0];
+  }
+
+  onMouseDown(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    if (e.target.className === 'bottom-right') {
+      const oldWidth = +(window.getComputedStyle(this.el).width).replace('px', '');
+      const oldHeight = +(window.getComputedStyle(this.el).height).replace('px', '');
+      document.onmousemove = me => {
+        this.el.style.width = `${oldWidth + me.x - e.x}px`;
+        this.el.style.height = `${oldHeight + me.y - e.y}px`;
+      }
+      return false;
+    }
+    if (e.target.className === 'bottom') {
+      const oldHeight = +(window.getComputedStyle(this.el).height).replace('px', '');
+      document.onmousemove = me => {
+        this.el.style.height = `${oldHeight + me.y - e.y}px`;
+      }
+      document.onmouseup = this.onMouseUp;
+      return false;
+    }
+    if (e.target.className === 'right') {
+      const oldWidth = +(window.getComputedStyle(this.el).width).replace('px', '');
+      document.onmousemove = me => {
+        this.el.style.width = `${oldWidth + me.x - e.x}px`;
+      }
+      document.onmouseup = this.onMouseUp;
+      return false;
+    }
+    
+    const [x, y] = this.getXY(this.el.style.transform);
+    document.onmousemove = moveEvent => this.onDragMove(moveEvent, [e.clientX - x, e.clientY - y]);
+    document.onmouseup = this.onMouseUp;
+  }
+
+  onDragMove(e, XY) {
+    this.el.style.transform = `translate(${e.clientX - XY[0]}px, ${e.clientY - XY[1]}px)`;
+  }
+
+  onMouseUp() {
+    document.onmousemove = null;
+  }
+}
+
+new SimpleDragResize(document.querySelector('.box'));
--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 ---- | ---- | ----
 [001-knot](./001-knot/index.html) | 自己画的一个中国结，教程：[用css画一个中国结](https://segmentfault.com/a/1190000016306150) | <img src="./001-knot/001-knot.png" width=200 />
 [002-simple-drag](./002-drag/index.html) | 一个简单的拖拽 | <img src="./002-drag/002-drag.gif" width=200 />
+[003-simple-drag-resize](./003-drag-resize/index.html) | 一个简单的拖拽和缩放 | <img src="./003-drag-resize/003.gif" width=200 />
 [065-loader](./065-loader/index.html) | 一个漂亮的 loading，教程：[前端每日实战：65](https://segmentfault.com/a/1190000015424389) | <img src="./065-loader/065-loading.gif" width=200 />
 [084-loader](./084-loader/index.html) | 一个漂亮的 loading，教程：[前端每日实战：84](https://segmentfault.com/a/1190000015700996) | <img src="./084-loader/084-loader.gif" width=200 />
 [098-css-pig](./098-dumb-pig/index.html) | 用 CSS 创作一只愤怒小鸟中的绿猪，教程：[前端每日实战：98](https://segmentfault.com/a/1190000015909608) | <img src="./098-dumb-pig/098-dumb-pig.png" width=200 />
